95232eeaf77de9d4f250acbeb6531b120cde4111
José Panero <panerootero@gmail.com>
2020-07-28 18:46:46 +0200

    Cambios realizados

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,22 +6,13 @@ export const environment = {
   production: false,
   urlGoty: 'http://localhost:5000/game-of-year-firebase/us-central1',
   firebase: {
-    apiKey: "AIzaSyCHFIa5JTe7VainGRR83mOjeeJd8g67_ic",
-    authDomain: "game-of-year-firebase.firebaseapp.com",
-    databaseURL: "https://game-of-year-firebase.firebaseio.com",
-    projectId: "game-of-year-firebase",
-    storageBucket: "game-of-year-firebase.appspot.com",
-    messagingSenderId: "163139495930",
-    appId: "1:163139495930:web:39b9ecffeb3c563c98a643"
+    apiKey: '<your-key>',
+    authDomain: '<your-project-authdomain>',
+    databaseURL: '<your-database-URL>',
+    projectId: '<your-project-id>',
+    storageBucket: '<your-storage-bucket>',
+    messagingSenderId: '<your-messaging-sender-id>'
   }
-  // firebase: {
-  //   apiKey: '<your-key>',
-  //   authDomain: '<your-project-authdomain>',
-  //   databaseURL: '<your-database-URL>',
-  //   projectId: '<your-project-id>',
-  //   storageBucket: '<your-storage-bucket>',
-  //   messagingSenderId: '<your-messaging-sender-id>'
-  // }
 };
 
 /*
